bc1bdcdffb5fea0a482f0292d9ad40fc7bc1039f
spensur <104689384+spensur@users.noreply.github.com>
2026-04-15 00:02:05 +0800

    Fixed misspelling

--- src/sections/Section1.jsx
+++ src/sections/Section1.jsx
@@ -5,8 +5,8 @@ import FrameR from "../assets/section-1/frame-r.webp";
 import FrameT from "../assets/section-1/frame-t.webp";
 
 import Lantern from "../assets/section-1/lantern.webp";
-import TassleJade from "../assets/section-1/tassle-jade.webp";
-import TassleTinghun from "../assets/section-1/tassle-tinghun.webp";
+import TasselJade from "../assets/section-1/tassel-jade.webp";
+import TasselTinghun from "../assets/section-1/tassel-tinghun.webp";
 
 import BackgroundBuildings from "../assets/section-1/background-buildings.webp";
 
@@ -18,8 +18,8 @@ export function Section1() {
       <img src={FrameR} className="absolute" style={{ right: '-12%', top: '5%',  width: '50%' }} />
 
       <img src={Lantern} className="absolute" style={{ left: '-1%', top: '25.5%',  width: '23%' }} />
-      <img src={TassleJade} className="absolute" style={{ right: '9%', top: '18%',  width: '13%' }} />
-      <img src={TassleTinghun} className="absolute" style={{ right: '-1%', top: '28%',  width: '18%' }} />
+      <img src={TasselJade} className="absolute" style={{ right: '9%', top: '18%',  width: '13%' }} />
+      <img src={TasselTinghun} className="absolute" style={{ right: '-1%', top: '28%',  width: '18%' }} />
 
       <img src={BorderBlueFlowerL} className="absolute" style={{ left: '-24%', top: '-9%', width: '75%' }} />
       <img src={BorderWhiteFlowerR} className="absolute" style={{ right: '-25%', top: '-8%',  width: '75%' }} />
