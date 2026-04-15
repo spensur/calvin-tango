24048171916219208d16734e7680728c4fe3902d
spensur-rog <104689384+spensur@users.noreply.github.com>
2026-04-15 14:21:48 +0800

    Grouped elements and added meta for iphone viewing

--- src/sections/Section1.jsx
+++ src/sections/Section1.jsx
@@ -12,31 +12,43 @@ import BackgroundBuildings from "../assets/section-1/background-buildings.webp";
 
 export function Section1() {
   return (
-    <section className="relative w-full overflow-visible section-1-bg" style={{ aspectRatio: '9/16' }}>
+    <section className="relative w-full min-h-228 overflow-visible section-1-bg">
+      {/* --- FRAME --- */}
       <img src={FrameT} className="absolute" style={{ left: '13%', top: '0%',  width: '100%' }} />
-      <img src={FrameL} className="absolute" style={{ left: '-10.5%', top: '5%',  width: '50%' }} />
-      <img src={FrameR} className="absolute" style={{ right: '-12%', top: '5%',  width: '50%' }} />
+      <img src={FrameL} className="absolute" style={{ left: '-10%', top: '3%',  width: '50%' }} />
+      <img src={FrameR} className="absolute" style={{ right: '-12.1%', top: '3%',  width: '50%' }} />
 
-      <img src={Lantern} className="absolute" style={{ left: '-1%', top: '25.5%',  width: '23%' }} />
-      <img src={TasselJade} className="absolute" style={{ right: '9%', top: '18%',  width: '13%' }} />
-      <img src={TasselTinghun} className="absolute" style={{ right: '-1%', top: '28%',  width: '18%' }} />
+      {/* --- TOP LEFT GRAPHICS --- */}
+      <div className="relative left-0 -top-20">
+        <img src={Lantern} className="absolute left-0 top-60 w-24" />
+        <img src={BorderBlueFlowerL} className="absolute -left-19 w-75" />
+      </div>
 
-      <img src={BorderBlueFlowerL} className="absolute" style={{ left: '-24%', top: '-9%', width: '75%' }} />
-      <img src={BorderWhiteFlowerR} className="absolute" style={{ right: '-25%', top: '-8%',  width: '75%' }} />
+      {/* --- TOP RIGHT GRAPHICS --- */}
+      <div className="relative right-0 -top-15">
+        <img src={TasselJade} className="absolute right-13 top-46 w-13" />
+        <img src={TasselTinghun} className="absolute right-2 top-67 w-18" />
+        <img src={BorderWhiteFlowerR} className="absolute -right-21 w-75" />
+      </div>
 
 
-      <h3 className="w-64 text-center top-42 left-1/2 -translate-x-1/2 font-caudex absolute text-xs font-bold">
+      {/* --- TEXT --- */}
+      <h3 className="w-64 text-center top-34 left-1/2 -translate-x-1/2 font-caudex absolute text-xs font-bold 
+        bg-linear-to-t from-[#E1BF6A] to-[#C28C2A] bg-clip-text text-transparent drop-shadow-sm">
         YOU ARE CORDIALLY INVITED TO 
         CELEBRATE THE TINGHUN OF
       </h3>
 
-      <div className="text-center top-56 left-1/2 -translate-x-1/2 font-corinthia absolute font-bold">
+      <div className="text-center top-50 left-1/2 -translate-x-1/2 font-corinthia absolute font-bold overflow-visible p-1.5
+        bg-linear-to-t from-[#E1BF6A] to-[#C28C2A] bg-clip-text text-transparent drop-shadow-sm">
         <h1 className="text-7xl">Melvin</h1>
         <h1 className="text-5xl -mt-4">&</h1>
         <h1 className="text-7xl -mt-8">Camille</h1>
       </div>
 
 
+      {/* --- MELBIRD & CAMMY --- */}
+      <img src={BackgroundBuildings} className="absolute" style={{ left: '0%', bottom: '0%',  width: '100%' }} />
     </section>
   )
 }
