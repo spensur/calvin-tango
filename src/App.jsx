45d12433f33450174d77726241605806fcc94ce0
spensur-rog <104689384+spensur@users.noreply.github.com>
2026-04-16 19:03:12 +0800

    Finished section-4

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Section1 } from "./sections/Section1";
 import { Section2 } from "./sections/Section2";
 import { RepeatingBackground } from "./components/RepeatingBackground";
 import { Section3 } from "./sections/Section3";
+import { Section4 } from "./sections/Section4";
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Section1/>
       <Section2/>
       <Section3/>
+      <Section4/>
     </div>
   );
 }
---
+++ src/sections/Section4.jsx
@@ -0,0 +1,40 @@
+import CurtainFooterT from "../assets/section-4/curtain-footer-t.webp";
+import FrameT from "../assets/section-1/frame-t.webp";
+import Venue from "../assets/section-4/venue.webp";
+import LanternFlower from "../assets/section-4/lantern-flower.webp";
+import NativityStarPatchRating from "../assets/section-4/nativity-star-patch-rating.webp";
+
+import Footer from "../assets/section-4/footer.webp";
+
+
+export function Section4() {
+  return (
+    <section className="relative w-full min-h-200 overflow-visible mt-10">
+      {/* --- FOOTER --- */}
+      <img src={Footer} className="absolute left-0 bottom-0 scale-100"/>
+
+      {/* --- VENUE DETAILS --- */}
+      <div className="absolute w-75 left-1/2 -translate-x-1/2 top-78 font-caudex flex flex-col items-center">
+        <h1 className="text-3xl font-bold">East Ocean Palace</h1>
+        <p className="text-sm text-slate-600 -mt-1">Parañaque, Metro Manila</p>
+        <div className="text-sm -mt-1 flex text-slate-600 items-center gap-1">
+          4.6
+          <img src={NativityStarPatchRating} className="h-5"/>
+        </div>
+      </div>
+      
+      {/* --- VENUE PIC --- */}
+      <div className="absolute w-full h-75 top-0">
+        <div className="relative size-full">
+          <img src={Venue} className="absolute inset-0 w-full h-full object-cover"/>
+          <img src={CurtainFooterT} className="absolute left-0 -top-15 scale-100"/>
+
+          <img src={FrameT} className="absolute left-0 -bottom-2 scale-90"/>
+
+          <img src={LanternFlower} className="absolute -left-15 -bottom-11 -scale-x-80 scale-80"/>
+          <img src={LanternFlower} className="absolute -right-15 -bottom-11 scale-80"/>
+        </div>
+      </div>
+    </section>
+  );
+}
